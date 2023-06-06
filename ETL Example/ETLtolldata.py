da77ec80a98051f857eaec5fed3f40f9cd1cfb86
Cody Minns <crminns@gmail.com>
2023-06-06 16:48:58 -0600

    adding more to ETL example

--- ETL Example/ETLtolldata.py
+++ ETL Example/ETLtolldata.py
@@ -11,9 +11,9 @@ from airflow.utils.dates import days_ago
 # define DAG arguments
 
 default_args = {
-    'owner' : 'Fake McName',
+    'owner' : 'Cody Minns',
     'start_date' : days_ago(0),
-    'email' : 'mcname@fake.com',
+    'email' : 'crminns@gmail.com',
     'email_on_failure' : True,
     'email_on_retry' : True,
     'retries' : 1,
@@ -26,14 +26,14 @@ dag = DAG(
     'ETL_toll_data',
     schedule_interval = timedelta(days = 1),
     default_args = default_args,
-    description = 'Apache Airflow Final Assignment'
+    description = 'ETL pipeline for toll data from various sources'
 )
 
 # define the unzip data task
 
 unzip_data = BashOperator(
     task_id = 'unzip_data',
-    bash_command = 'tar -zxvf tolldata.tgz',
+    bash_command = 'tar -zxvf /$AIRFLOW_HOME/../data/tolldata.tgz -C /$AIRFLOW_HOME/../data/',
     dag = dag
 )
 
@@ -41,7 +41,7 @@ unzip_data = BashOperator(
 
 extract_data_from_csv = BashOperator(
     task_id = 'extract_data_from_csv',
-    bash_command = 'cut -d"," -f1-4 vehicle-data.csv > csv_data.csv',
+    bash_command = 'cut -d"," -f1-4 /$AIRFLOW_HOME/../data/vehicle-data.csv > /$AIRFLOW_HOME/../data/csv_data.csv',
     dag = dag
 )
 
@@ -49,7 +49,7 @@ extract_data_from_csv = BashOperator(
 
 extract_data_from_tsv = BashOperator(
     task_id = 'extract_data_from_tsv',
-    bash_command = 'cut -f5-7 tollplaza-data.tsv | tr "\t" ","  > tsv_data.csv',
+    bash_command = 'cut -f5-7 /$AIRFLOW_HOME/../data/tollplaza-data.tsv | tr "\t" ","  > /$AIRFLOW_HOME/../data/tsv_data.csv',
     dag = dag
 )
 
@@ -57,7 +57,7 @@ extract_data_from_tsv = BashOperator(
 
 extract_data_from_fixed_width = BashOperator(
     task_id = 'extract_data_from_fixed_width',
-    bash_command = 'cut -b58-66 payment-data.txt | tr " " "," > fixed_width_data.csv',
+    bash_command = 'cut -b59-66 /$AIRFLOW_HOME/../data/payment-data.txt | tr " " "," > /$AIRFLOW_HOME/../data/fixed_width_data.csv',
     dag = dag
 )
 
@@ -65,7 +65,7 @@ extract_data_from_fixed_width = BashOperator(
 
 consolidate_data = BashOperator(
     task_id = 'consolidate_data',
-    bash_command = 'paste csv_data.csv tsv_data.csv fixed_width_data.csv > extracted_data.csv',
+    bash_command = 'paste -d"," /$AIRFLOW_HOME/../data/csv_data.csv /$AIRFLOW_HOME/../data/tsv_data.csv /$AIRFLOW_HOME/../data/fixed_width_data.csv > /$AIRFLOW_HOME/../data/extracted_data.csv',
     dag = dag
 )
 
@@ -73,8 +73,7 @@ consolidate_data = BashOperator(
 
 transform_data = BashOperator(
     task_id = 'transform_data',
-    bash_command = 'paste <(cut -d"," -f1-3 extracted_data.csv) <(cut -d"," -f4 extracted_data.csv | tr ":lower:" ":upper:") \
-                          <(cut -d"," -f5- extracted_data.csv) > Staging/transformed_data.csv',
+    bash_command = 'paste -d"," <(cut -d"," -f1-3 /$AIRFLOW_HOME/../data/extracted_data.csv) <(cut -d"," -f4 /$AIRFLOW_HOME/../data/extracted_data.csv | tr [:lower:] [:upper:]) <(cut -d"," -f5- /$AIRFLOW_HOME/../data/extracted_data.csv) > /$AIRFLOW_HOME/../data/transformed_data.csv',
     dag = dag
 )
 
